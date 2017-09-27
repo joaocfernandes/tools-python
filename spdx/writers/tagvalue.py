34e1cff23f73d8366e7daf9864467108f00fb7c3
Hugo Drumond Jacob <Hugo.Jacob@bmw.de>
2017-09-27 11:49:06 +0200

    writers: Render extracted licenses ID as 'LicenseRef-XXX'
    The SPDX Specification states that extracted licenses shall be referred
    as LicenseRef-[ID].

--- spdx/writers/tagvalue.py
+++ spdx/writers/tagvalue.py
@@ -220,8 +220,9 @@ def write_extracted_licenses(lics, out):
     """
     Write extracted licenses fields to out.
     """
-    write_value('LicenseID', lics.identifier, out)
 
+    write_value('LicenseID', 'LicenseRef-' + lics.identifier, out)
+    write_text_value('ExtractedText', lics.text, out)
     if lics.full_name is not None:
         write_value('LicenseName', lics.full_name, out)
 
